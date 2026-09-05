e56b0d190126b698a966088a9e86c2a07778533b
Nikolai Matiushev <egao1980@gmail.com>
2026-09-05 16:34:42 +0100

    Register :xsd with schema-protocol emit-schema / parse-schema.

--- src/protocol.lisp
+++ src/protocol.lisp
@@ -1,5 +1,24 @@
 (in-package #:schema-protocol-xsd)
 
+(defclass xsd-schema-backend (schema-format-backend) ()
+  (:documentation "schema-protocol format backend for :xsd."))
+
+(defmethod backend-emit-schema ((backend xsd-schema-backend) schema
+                                &key (version :1.0) (as :xml) &allow-other-keys)
+  (declare (ignore backend))
+  (emit schema :version version :as as))
+
+(defmethod backend-parse-schema ((backend xsd-schema-backend) source
+                                 &key name package version &allow-other-keys)
+  (declare (ignore backend))
+  (apply #'compile-schema source
+         (append (when name (list :name name))
+                 (when package (list :package package))
+                 (when version (list :version version)))))
+
+(eval-when (:load-toplevel :execute)
+  (register-schema-format :xsd (make-instance 'xsd-schema-backend)))
+
 (defmethod xsd-schema ((schema symbol) &key (version :1.0))
   (emit schema :version version))
 
